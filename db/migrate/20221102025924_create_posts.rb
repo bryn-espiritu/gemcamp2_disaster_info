c5b5374487e8211602892c5d232889cbfbc174d2
brynex20 <brynespiritu@gmail.com>
2022-11-02 11:02:09 +0800

    migrate posts database (title, content, address

--- db/migrate/20221102025924_create_posts.rb
+++ db/migrate/20221102025924_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-
+      t.string :tittle
+      t.string :content
+      t.string :address
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_024250) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_025924) do
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "tittle"
+    t.string "content"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
