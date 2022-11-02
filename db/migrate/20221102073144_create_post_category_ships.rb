1ed223164316acf054a8e2cadcb1bacc1bb7483a
brynex20 <brynespiritu@gmail.com>
2022-11-02 15:49:03 +0800

    migrate categories, post_category_ship

--- db/migrate/20221102073144_create_post_category_ships.rb
+++ db/migrate/20221102073144_create_post_category_ships.rb
@@ -1,7 +1,8 @@
 class CreatePostCategoryShips < ActiveRecord::Migration[7.0]
   def change
     create_table :post_category_ships do |t|
-
+      t.references :category
+      t.references :post
       t.timestamps
     end
   end
