0f67e3f16d5f4176663253bdc61bc05d5a43e123
brynex20 <brynespiritu@gmail.com>
2022-11-02 13:03:31 +0800

    completed post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,63 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+  before_action :validate_post_user, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.includes(:user).all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    ;
+  end
+
+  def show
+    ;
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :address)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def validate_post_user
+    unless @post.user == current_user
+      flash[:notice] = 'Post Error Unauthorized access'
+      redirect_to post_path
+    end
+  end
+
 end
