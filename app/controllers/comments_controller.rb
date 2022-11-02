631d3e4049135ed781acafcebea9191c3b82f707
brynex20 <brynespiritu@gmail.com>
2022-11-02 13:41:44 +0800

    add index method(controller) and generate index file(views)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  before_action set_post
+
+  def index
+    @comments = @post.comments
+  end
+
+  private
+
+  def set_post
+    @post = POst.find params[:post_id]
+  end
 end
