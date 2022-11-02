cc5c1da2dcd99b0cd51648bcb24a86d179990bf8
brynex20 <brynespiritu@gmail.com>
2022-11-02 18:20:51 +0800

    added comment conts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_080308) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_101037) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_080308) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "delete_at"
+    t.integer "comments_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
