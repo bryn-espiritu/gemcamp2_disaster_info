bfa0bfaa364de27144ff453cd3dec19832bc8713
brynex20 <brynespiritu@gmail.com>
2022-11-04 17:43:43 +0800

    add validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   before_action :set_post
   before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :validate_comment_owner, only: [:edit, :update, :destroy]
 
   def index
     @comments = @post.comments
@@ -12,6 +14,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
     if @comment.save
       redirect_to post_comments_path(params[:post_id])
     else
@@ -25,6 +28,7 @@ class CommentsController < ApplicationController
 
   def update
     if @comment.update(comment_params)
+      flash[:notice] = "Comment Updated"
       redirect_to post_comments_path(@post)
     else
       render :edit
@@ -33,7 +37,9 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment.destroy
+    flash[:alert] = "Comment Deleted"
     redirect_to post_comments_path(@post)
+
   end
 
   private
@@ -46,10 +52,18 @@ class CommentsController < ApplicationController
     @comment = @post.comments.find(params[:id])
   end
 
-def comment_params
-  params.require(:comment)
-        .permit(:content)
-        .merge(post_id: params[:post_id])
-end
+  def comment_params
+    params.require(:comment)
+          .permit(:content)
+          .merge(post_id: params[:post_id])
+  end
+
+  def validate_comment_owner
+    unless @comment.user == current_user
+      flash[:notice] = 'the comment not belongs to you'
+      redirect_to post_comments_path(@post)
+    end
+  end
+
 
 end
