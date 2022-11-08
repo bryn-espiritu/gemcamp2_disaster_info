b0f511e96deb45ca26ccb8986d512cec5513f9b0
brynex20 <brynespiritu@gmail.com>
2022-11-08 17:13:08 +0800

    shorten url for post_new, post_edit and comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-   root "posts#index"
-  resources :posts do
-    resources :comments, except: :show
+
+  match '9872', to: "posts#index", as: 'posts', via: :all
+  root "posts#index"
+  resources :posts, path_names: { new: '1234', edit: '4321' } do
+    scope(path_names: { new: '5897', edit: '9752' }) do
+      resources :comments, path: '4854'
+    end
   end
+
   resources :categories
 end
