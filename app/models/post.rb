6aa40b6c09f6806b14a0ecd88945b3b0626c1869
brynex20 <brynespiritu@gmail.com>
2022-11-11 14:06:13 +0800

    validate unique_num, uniqueness validate ip_address, presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :address, presence: true
+  validates :ip_address, presence: true
+  validates :unique_num, uniqueness: true
   belongs_to :user
   has_many :comments
   has_many :post_category_ships
@@ -10,4 +12,15 @@ class Post < ApplicationRecord
   # extend FriendlyId
   # # friendly_id :title, use: :slugged
 
+  after_validation :generate_short_string
+
+  private
+
+  def generate_short_string
+    loop do
+      @string_unique = sprintf "%04d", rand(-9999)
+      break unless Post.exists?(unique_num: unique_num)
+    end
+    self.unique_num = @string_unique
+  end
 end
