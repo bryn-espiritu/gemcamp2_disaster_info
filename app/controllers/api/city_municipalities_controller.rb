551ba5a4014ad0cf6b78b2886d2c87393b698aca
brynex20 <brynespiritu@gmail.com>
2022-11-15 19:23:13 +0800

    change find to find_by params in api CityMunicipality Controller

--- app/controllers/api/city_municipalities_controller.rb
+++ app/controllers/api/city_municipalities_controller.rb
@@ -1,13 +1,12 @@
 class Api::CityMunicipalitiesController < ApplicationController
   def index
-    if province = Address::Province.find(params[:province_id])
+    if province = Address::Province.find_by(params[:province_id])
       city_municipalities = province.city_municipalities
       render json: city_municipalities, each_serializer: CityMunicipalitySerializer
     else
-      district = Address::District.find(params[:district_id])
+      district = Address::District.find_by(params[:district_id])
       city_municipalities = district.city_municipalities
       render json: city_municipalities, each_serializer: CityMunicipalitySerializer
     end
-
   end
 end
