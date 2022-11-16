224ac7e2c510af42390442949ee7299662c37baf
brynex20 <brynespiritu@gmail.com>
2022-11-16 16:32:43 +0800

    add reference to posts

---
+++ db/migrate/20221116031727_add_column_to_posts.rb
@@ -0,0 +1,10 @@
+class AddColumnToPosts < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :posts, :address_region
+    add_reference :posts, :address_province
+    add_reference :posts, :address_district
+    add_reference :posts, :address_city_municipality
+    add_reference :posts, :address_barangay
+
+  end
+end
